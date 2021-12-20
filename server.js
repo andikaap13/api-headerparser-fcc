741df1485e7d3f1be12b33982cc6c5dee101bbd4
andikaap13 <50222017+andikaap13@users.noreply.github.com>
2021-12-20 20:43:27 +0700

    Update license

--- server.js
+++ server.js
@@ -25,7 +25,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  var ip = '';
 
+  res.json({
+    ipaddress: 'hello API',
+    language: '',
+    software: ''
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
